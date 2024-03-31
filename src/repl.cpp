1d78159e54b9d619353ef174fb7edb22e77b8734
aym-n <ajm8982@gmail.com>
2024-03-31 11:16:12 +0530

    🖱️: Update Cursor

--- src/repl.cpp
+++ src/repl.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 
 #include "repl.h"
+#include "bTree.h"
 
 using namespace std;
 
@@ -118,13 +119,9 @@ void close_input_buffer(InputBuffer *input_buffer)
 
 // till now no issue
 void* cursor_value(Cursor* cursor) {
-    uint32_t row_num = cursor->row_num;
-    uint32_t page_num = row_num / 100;   // eg row_num = 1000,ROWS_PER_PAGE = 499, page_num = 2
-    Pager* pager = cursor->table->pager; // Get the pager instance
-    void* page = pager->get_page(page_num);
-    uint32_t row_offset = row_num % 100;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-    return (char *)page + byte_offset; // page = 0x1234, byte_offset = 1000, return 0x1234 + 1000
+    uint32_t page_num = cursor->page_num;
+    void* page = cursor->table->pager->get_page(page_num);
+    return leaf_node_value(page, cursor->cell_num);
 }
 // checked
 ExecuteResult execute_insert(Statement *statement, Table *table)
@@ -329,22 +326,32 @@ Cursor *table_start(Table *table)
 {
     Cursor *cursor = new Cursor;
     cursor->table = table;
-    cursor->row_num = 0;
-    cursor->end_of_table = (table->num_rows == 0);
+    cursor->page_num = table->root_page_num;
+    cursor->cell_num = 0;
+
+    void *root_node = table->pager->get_page(table->root_page_num);
+    uint32_t num_cells = *leaf_node_num_cells(root_node);
+    cursor->end_of_table = (num_cells == 0);
     return cursor;
 }
 Cursor *table_end(Table *table)
 {
     Cursor *cursor = new Cursor;
     cursor->table = table;
-    cursor->row_num = table->num_rows;
+    cursor->page_num = table->root_page_num;    
+    void *root_node = table->pager->get_page(table->root_page_num);
+    uint32_t num_cells = *leaf_node_num_cells(root_node);
+    cursor->cell_num = num_cells;
     cursor->end_of_table = true;
     return cursor;
 }
 void cursor_advance(Cursor *cursor)
 {
-    cursor->row_num += 1;
-    if (cursor->row_num >= cursor->table->num_rows)
+    uint32_t page_num = cursor->page_num;
+    void *node = cursor->table->pager->get_page(page_num);
+
+    cursor->cell_num += 1;
+    if (cursor->cell_num >= (*leaf_node_num_cells(node)))
     {
         cursor->end_of_table = true;
     }
